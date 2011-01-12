facc025497472d004ed57f254c710ff85df1f613
Carl Vondrick <cvondric@ics.uci.edu>
2011-01-11 22:08:04 -0800

    first distribution

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     author_email = "cvondric@ics.uci.edu",
     description = "A concise computer vision toolkit",
     license = "MIT",
-    version = "0.1.0",
+    version = "0.0.1",
     classifiers = ['Development Status :: 1 - Planning',
                    'Intended Audience :: Developers'],
     packages = ['vision', 'vision.track', 'vision.ffmpeg'],
