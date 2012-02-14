22d60fed9963d8338502dc05903a56eef5686537
Carl Vondrick <vondrick@mit.edu>
2012-02-14 14:32:04 -0500

    super fast 3d convolution

--- vision/track/realcoords.pyx
+++ vision/track/realcoords.pyx
@@ -16,10 +16,13 @@ def track(video, seeds, patches, projections, double delta = 10e-4):
     cdef double xmin, xmax, ymin, ymax, zmin, zmax
     cdef double x, y, z
     cdef int xi, yi, zi
-    cdef double normalizer, points
+    cdef double normalizer
     cdef int contains
+    cdef int sxtl, sytl, sxbr, sybr
+    cdef double px, py, pn
     cdef annotations.Box seed
     cdef numpy.ndarray[numpy.double_t, ndim=3] mapping
+    cdef numpy.ndarray[numpy.double_t, ndim=2] matrix
 
     (xmin, xmax), (ymin, ymax), (zmin, zmax) = pmvs.get_patch_bounds(patches)
 
@@ -28,27 +31,26 @@ def track(video, seeds, patches, projections, double delta = 10e-4):
     logger.debug("z-bounds are: {0} thru {1}".format(zmin, zmax))
 
     logger.debug("Building 3D density")
-    mapping = numpy.empty(((xmax - xmin + 1) / delta,
+    mapping = numpy.zeros(((xmax - xmin + 1) / delta,
                            (ymax - ymin + 1) / delta,
                            (zmax - zmin + 1) / delta))
     normalizer = 0
-    for x from xmin <= x <= xmax by delta: 
-        for y from ymin <= y <= ymax by delta:
-            for z from zmin <= z <= zmax by delta:
-                points = 0
-                real = numpy.array([x, y, z, 1])
-                for seed in seeds:
-                    point = projections[seed.frame].project(real)
-                    contains = (seed.xtl >= point[0] and seed.xbr <= point[0] and
-                                seed.ytl >= point[1] and seed.ybr <= point[1])
-                    if contains:
-                        points += 1.0
-                xi = <int> ((x - xmin) / delta)
-                yi = <int> ((y - ymin) / delta)
-                zi = <int> ((z - zmin) / delta)
-                mapping[xi, yi, zi] = points
-                normalizer += points
+    for seed in seeds:
+        matrix = projections[seed.frame].matrix
+        for x from xmin <= x <= xmax by delta: 
+            for y from ymin <= y <= ymax by delta:
+                for z from zmin <= z <= zmax by delta:
+                    pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
+                    px = (matrix[0,0]*x + matrix[0,1]*y +matrix[0,2]*z + matrix[0,3]) / pn
+                    py = (matrix[1,0]*x + matrix[1,1]*y +matrix[1,2]*z + matrix[1,3]) / pn
+                    if seed.xtl <= px and seed.xbr >= px and seed.ytl <= py and seed.ybr >= py:
+                        xi = <int> ((x - xmin) / delta)
+                        yi = <int> ((y - ymin) / delta)
+                        zi = <int> ((z - zmin) / delta)
+                        mapping[xi, yi, zi] += 1
+                        normalizer += 1
     if normalizer > 0:
         mapping = mapping / normalizer
+        logger.debug("Normalizer is {0}".format(normalizer))
     else:
         raise RuntimeError("Normalizer is 0, probably no points mapped")
