44f7342cd13566005a854be36d1925600c33d6ae
Carl Vondrick <vondrick@mit.edu>
2012-02-17 04:11:45 -0500

    real coords

--- experiments/realcoords.py
+++ experiments/realcoords.py
@@ -6,33 +6,33 @@ import random
 
 logging.basicConfig(level = logging.DEBUG)
 
-#path = ("/csail/vision-videolabelme/databases/"
-#        "video_adapt/home_ac_a/frames/0/bundler")
-#path = "/csail/vision-videolabelme/databases/video_adapt/demos/bottle_table/bundler"
-#
-#video = vision.flatframeiterator(path, 1, 5)
-#
-#root = os.path.join(path, "pmvs")
-#patches, projections = pmvs.read(root)
-#
-#seed = vision.Box(173, 30, 173 + 51, 30 + 137, 0)
-#seed2 = vision.Box(256, 49, 256 + 58, 49 + 117, 200 / 5)
-#seed3 = vision.Box(173, 88, 173 + 67, 88 + 89, 600 / 5)
-#seed4 = vision.Box(156, 57, 156 + 71, 57 + 115, 900 / 5)
-#seed5 = vision.Box(184, 24, 184 + 70, 24 + 146, 840 / 5)
-#badseed = vision.Box(358, 12, 358 + 33, 12 + 25, 150 / 5)
-#predicted = track(video, [seed, seed2, seed3, seed4, seed5], patches, projections)
-
 path = ("/csail/vision-videolabelme/databases/"
-        "video_adapt/home_ac_a/frames/5/bundler-5")
+        "video_adapt/home_ac_a/frames/0/bundler")
+path = "/csail/vision-videolabelme/databases/video_adapt/demos/bottle_table/bundler"
 
 video = vision.flatframeiterator(path, 1, 5)
 
 root = os.path.join(path, "pmvs")
 patches, projections = pmvs.read(root)
 
-seed = vision.Box(55, 39, 55 + 270, 39 + 136, 145)
-predicted = track(video, [seed], patches, projections)
+seed = vision.Box(173, 30, 173 + 51, 30 + 137, 0)
+seed2 = vision.Box(256, 49, 256 + 58, 49 + 117, 200 / 5)
+seed3 = vision.Box(173, 88, 173 + 67, 88 + 89, 600 / 5)
+seed4 = vision.Box(156, 57, 156 + 71, 57 + 115, 900 / 5)
+seed5 = vision.Box(184, 24, 184 + 70, 24 + 146, 840 / 5)
+badseed = vision.Box(358, 12, 358 + 33, 12 + 25, 150 / 5)
+predicted = track(video, [seed, seed2], patches, projections)
 
-vit = visualize.highlight_path(video, predicted)
-visualize.save(vit, lambda x: "tmp/path{0}.jpg".format(x))
+#path = ("/csail/vision-videolabelme/databases/"
+#        "video_adapt/home_ac_a/frames/5/bundler-5")
+#
+#video = vision.flatframeiterator(path, 1, 5)
+#
+#root = os.path.join(path, "pmvs")
+#patches, projections = pmvs.read(root)
+#
+#seed = vision.Box(55, 39, 55 + 270, 39 + 136, 145)
+#predicted = track(video, [seed], patches, projections)
+#
+#vit = visualize.highlight_path(video, predicted)
+#visualize.save(vit, lambda x: "tmp/path{0}.jpg".format(x))
--- vision/track/realcoords.pyx
+++ vision/track/realcoords.pyx
@@ -2,6 +2,7 @@ import logging
 from vision.reconstruction import pmvs
 import numpy
 import vision
+import matplotlib.pyplot as plt
 from vision.reconstruction import plywriter
 
 cimport numpy
@@ -9,7 +10,10 @@ from vision cimport annotations
 
 logger = logging.getLogger("vision.track.realcoords")
 
-def track(video, seeds, patches, projections, double delta = 10e-3 * 2):
+cdef extern from "math.h":
+    float exp(float n)
+
+def track(video, seeds, patches, projections, double delta = 10e-3):
     """
     This tracker uses a 3D reconstruction of a scene in order to
     localize objects throughout a video sequence.
@@ -18,11 +22,14 @@ def track(video, seeds, patches, projections, double delta = 10e-3 * 2):
     cdef double x, y, z
     cdef int xi, yi, zi
     cdef int xs, ys, zs
-    cdef double normalizer
+    cdef int width, height
+    cdef double normalizer, prob3d
     cdef double px, py, pn
+    cdef int pxi, pyi, pxii, pyii
     cdef annotations.Box seed
-    cdef numpy.ndarray[numpy.double_t, ndim=3] mapping 
+    cdef numpy.ndarray[numpy.double_t, ndim=3] mapping, intersection
     cdef numpy.ndarray[numpy.double_t, ndim=2] matrix
+    cdef numpy.ndarray[numpy.double_t, ndim=2] prob2map, intersection2map
 
     bounds = pmvs.get_patch_bounds(patches)
     (xmin, xmax), (ymin, ymax), (zmin, zmax) = bounds
@@ -38,6 +45,7 @@ def track(video, seeds, patches, projections, double delta = 10e-3 * 2):
     logger.debug("Trying to allocate {0}x{1}x{2}={3}".format(xs, ys, zs, 
                                                              xs*ys*zs))
     mapping = numpy.zeros((xs, ys, zs))
+    intersection = numpy.ones((xs, ys, zs))
     logger.debug("Allocated")
     normalizer = 0
     for seednum, seed in enumerate(seeds):
@@ -50,30 +58,88 @@ def track(video, seeds, patches, projections, double delta = 10e-3 * 2):
                 for zi in range(zs):
                     z = zi * delta + zmin
                     pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
+                    if pn < 0:
+                        intersection[xi, yi, zi] = 0
+                        continue
                     px = (matrix[0,0]*x + matrix[0,1]*y +matrix[0,2]*z + matrix[0,3]) / pn
                     py = (matrix[1,0]*x + matrix[1,1]*y +matrix[1,2]*z + matrix[1,3]) / pn
                     if seed.xtl <= px and seed.xbr >= px and seed.ytl <= py and seed.ybr >= py:
                         mapping[xi, yi, zi] += 1
                         normalizer += 1
+                    else:
+                        intersection[xi, yi, zi] = 0
+
     if normalizer > 0:
         mapping = mapping / normalizer
         logger.debug("Normalizer is {0}".format(normalizer))
     else:
         raise RuntimeError("Normalizer is 0, probably no points mapped")
+    
+    cdef double sigma = 0.0001
+    normalizer = 0
+    for xi in range(xs):
+        for yi in range(ys):
+            for zi in range(zs):
+                e = exp(mapping[xi, yi, zi] / sigma)
+                normalizer += e
+                mapping[xi, yi, zi] = e
 
-#    for frame, projection in enumerate(projections):
-#        matrix = projections[frame].matrix
-#        for x from xmin <= x <= xmax by delta: 
-#            for y from ymin <= y <= ymax by delta:
-#                for z from zmin <= z <= zmax by delta:
-#                    xi = <int> ((x - xmin) / delta)
-#                    yi = <int> ((y - ymin) / delta)
-#                    zi = <int> ((z - zmin) / delta)
-#                    prob3d = mapping[xi, yi, zi]
-#                    pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
-#                    px = (matrix[0,0]*x + matrix[0,1]*y +matrix[0,2]*z + matrix[0,3]) / pn
-#                    py = (matrix[1,0]*x + matrix[1,1]*y +matrix[1,2]*z + matrix[1,3]) / pn
+    cdef int radius = 10
+    for frame, projection in enumerate(projections):
+        logger.debug("Projecting into {0}".format(frame))
+        matrix = projections[frame].matrix
+        prob2map = numpy.zeros(video[frame].size)
+        intersection2map = numpy.zeros(video[frame].size)
+        width, height = video[frame].size
+        normalizer = 0
+        points = []
+        for x from xmin <= x <= xmax by delta: 
+            for y from ymin <= y <= ymax by delta:
+                for z from zmin <= z <= zmax by delta:
+                    xi = <int> ((x - xmin) / delta)
+                    yi = <int> ((y - ymin) / delta)
+                    zi = <int> ((z - zmin) / delta)
+                    prob3d = mapping[xi, yi, zi]
+                    pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
+                    if pn < 0:
+                        continue
+                    pxi = <int>((matrix[0,0]*x+matrix[0,1]*y+matrix[0,2]*z+matrix[0,3])/pn)
+                    pyi = <int>((matrix[1,0]*x+matrix[1,1]*y+matrix[1,2]*z+matrix[1,3])/pn)
+                    for pxii in range(pxi - radius, pxi + radius + 1):
+                        for pyii in range(pyi - radius, pyi + radius + 1):
+                            if pxii < 0 or pyii < 0 or pxii >= width or pyii >= height:
+                                continue
+                            prob2map[pxii, pyii] += prob3d > 0
+                            normalizer += prob3d
+                            if intersection[xi, yi, zi]:
+                                intersection2map[pxii, pyii] = max(intersection2map[pxii, pyii], 1)
+        if normalizer > 0:
+            prob2map = prob2map / normalizer
+
+            logger.debug("Ploting")
+            plt.subplot(221)
+            plt.title("Prob of object")
+            plt.set_cmap("gray")
+            plt.title("min = {0}, max = {1}".format(prob2map.min(), prob2map.max()))
+            plt.imshow(prob2map.transpose())
+            plt.subplot(222)
+            plt.title("Frame")
+            plt.imshow(numpy.asarray(video[frame]))
+            plt.subplot(223)
+            plt.title("Prob mask")
+            plt.imshow(numpy.array(numpy.asarray(video[frame]) * numpy.tile(prob2map / prob2map.max(), (3, 1, 1)).T, numpy.uint8))
+            plt.subplot(224)
+            plt.title("Intersection mask")
+            plt.imshow(numpy.array(numpy.asarray(video[frame]) * numpy.tile(intersection2map, (3, 1, 1)).T, numpy.uint8))
+            plt.savefig("tmp/prob{0}.png".format(frame))
+            plt.clf()
+        else:
+            logger.warning("Normalizer for frame {0} is 0".format(frame))
 
     logger.debug("Writing 3D probability map")
     plywriter.write(open("mapping.ply", "w"), mapping,
+        condition = plywriter.filterlower, bounds = bounds)
+
+    logger.debug("Writing intersection map")
+    plywriter.write(open("intersection.ply", "w"), intersection,
         condition = plywriter.filterlower, bounds = bounds)
