fba7379ad8a8934a1555f3da75c47b6b69be3271
Carl Vondrick <vondrick@mit.edu>
2012-02-22 17:18:55 -0500

    real prior 3d

--- vision/track/realprior.pyx
+++ vision/track/realprior.pyx
@@ -13,68 +13,78 @@ logger = logging.getLogger("vision.track.realcoords")
 cdef extern from "math.h":
     float exp(float n)
 
-def track(video, seeds, patches, projections):
-    """
-    This tracker uses a 3D reconstruction of a scene in order to
-    localize objects throughout a video sequence.
-    """
-    cdef double x, y, z
-    cdef int pxi, pxii, pyi, pyii
-    cdef int width, height
-    cdef int xmin, xmax, ymin, ymax
-    cdef double normalizer, normalizer2d, score, prob3d
-    cdef double px, py, pn
-    cdef annotations.Box seed
-    cdef numpy.ndarray[numpy.double_t, ndim=2] matrix
-    cdef numpy.ndarray[numpy.double_t, ndim=2] prob2map
+class ThreeD(object):
+    def __init__(self, video, seeds, patches, projections):
+        """
+        This tracker uses a 3D reconstruction of a scene in order to
+        localize objects throughout a video sequence.
+        """
+        self.video = video
+        self.seeds = seeds
+        self.patches = patches
+        self.projections = projections
 
-    logger.info("Cleaning seeds")
-    useseeds = []
-    for seed in seeds:
-        if seed.frame not in projections:
-            logger.warning("Dropping seed {0} because no projection".format(seed.frame))
-        else:
-            useseeds.append(seed)
-    seeds = useseeds
+    def build(self):
+        cdef double x, y, z
+        cdef double normalizer, score 
+        cdef double px, py, pn
+        cdef annotations.Box seed
+        cdef numpy.ndarray[numpy.double_t, ndim=2] matrix
 
-    if not seeds:
-        raise RuntimeError("No seeds")
+        logger.info("Cleaning seeds")
+        useseeds = []
+        for seed in self.seeds:
+            if seed.frame not in self.projections:
+                logger.warning("Dropping seed {0} because no projection".format(seed.frame))
+            else:
+                useseeds.append(seed)
+        seeds = useseeds
 
-    logger.info("Using seeds in {0}".format(", ".join(str(x.frame) for x in seeds)))
+        if not seeds:
+            raise RuntimeError("No seeds")
 
-    logger.info("Voting in 3-space")
-    mapping = {}
-    intersection = set()
-    for patch in patches:
-        score = 0
-        for seed in seeds:
-            matrix = projections[seed.frame].matrix
-            x, y, z, _ = patch.realcoords
-            pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
-            if pn < 0:
-                continue
-            px = (matrix[0,0]*x + matrix[0,1]*y +matrix[0,2]*z + matrix[0,3]) / pn
-            py = (matrix[1,0]*x + matrix[1,1]*y +matrix[1,2]*z + matrix[1,3]) / pn
-            if seed.xtl <= px and seed.xbr >= px and seed.ytl <= py and seed.ybr >= py:
-                score += 1
-        if score > 0:
-            normalizer += score
-            mapping[x, y, z] = score
-        if score == len(seeds):
-            intersection.add((x, y, z))
+        logger.info("Using seeds in {0}".format(", ".join(str(x.frame) for x in seeds)))
 
-    cdef int radius = 10
-    boxes = []
-    logger.info("Projecting votes into 2-space")
-    for frame, projection in sorted(projections.items()):
-        matrix = projection.matrix
-        videoframe = video[frame]
+        logger.info("Voting in 3-space")
+        self.mapping = {}
+        for patch in self.patches:
+            score = 0
+            for seed in seeds:
+                matrix = self.projections[seed.frame].matrix
+                x, y, z, _ = patch.realcoords
+                pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
+                if pn < 0:
+                    continue
+                px = (matrix[0,0]*x + matrix[0,1]*y +matrix[0,2]*z + matrix[0,3]) / pn
+                py = (matrix[1,0]*x + matrix[1,1]*y +matrix[1,2]*z + matrix[1,3]) / pn
+                if seed.xtl <= px and seed.xbr >= px and seed.ytl <= py and seed.ybr >= py:
+                    score += 1
+            if score > 0:
+                normalizer += score
+                self.mapping[x, y, z] = score
+        self.normalizer = normalizer
+
+    def score(self, frame, int radius = 10):
+        cdef numpy.ndarray[numpy.double_t, ndim=2] prob2map
+        cdef numpy.ndarray[numpy.double_t, ndim=2] matrix
+        cdef int pxi, pxii, pyi, pyii
+        cdef double x, y, z
+        cdef double normalizer2d, normalizer, prob3d
+
+        videoframe = self.video[frame]
         prob2map = numpy.zeros(videoframe.size)
-        width, height = video[frame].size
+
+        if frame not in self.projections:
+            return prob2map
+
+        projection = self.projections[frame]
+        matrix = projection.matrix
+        prob2map = numpy.zeros(videoframe.size)
+        width, height = videoframe.size
         points = []
         normalizer2d = 0
-
-        for (x, y, z), prob3d in mapping.iteritems():
+        normalizer = self.normalizer
+        for (x, y, z), prob3d in self.mapping.iteritems():
             prob3d = prob3d / normalizer
             pn = matrix[2,0]*x + matrix[2,1]*y +matrix[2,2]*z + matrix[2,3]
             if pn < 0:
@@ -88,7 +98,4 @@ def track(video, seeds, patches, projections):
                     prob2map[pxii, pyii] += prob3d
                     normalizer2d += prob3d
         prob2map /= normalizer2d
-
-
-        boxes.append(vision.Box(xmin, ymin, xmax, ymax, frame))
-    return boxes
+        return prob2map
